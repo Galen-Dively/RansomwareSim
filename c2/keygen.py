02aa14951c84a75c5e34c5fe95a8a16cd16be2dd
Galen <65838051+Galen-Dively@users.noreply.github.com>
2026-03-24 15:03:56 -0400

    Comment out save_keypair function in keygen.py
    Comment out the save_keypair function and its file operations.

--- c2/keygen.py
+++ c2/keygen.py
@@ -8,18 +8,18 @@ def create_keypair():
     public_key = private_key.public_key()
     return private_key, public_key
 
-def save_keypair(private_key: rsa.RSAPrivateKey, public_key: rsa.RSAPublicKey):
-    private_pem = private_key.private_bytes(encoding=serialization.Encoding.PEM,
-                                            format=serialization.PrivateFormat.PKCS8,
-                                            encryption_algorithm=serialization. NoEncryption()
-                                            )
-    public_pem = public_key.public_bytes(encoding=serialization.Encoding.PEM,
-                                         format=serialization.PublicFormat.SubjectPublicKeyInfo
-                                        )
+# def save_keypair(private_key: rsa.RSAPrivateKey, public_key: rsa.RSAPublicKey):
+#     private_pem = private_key.private_bytes(encoding=serialization.Encoding.PEM,
+#                                             format=serialization.PrivateFormat.PKCS8,
+#                                             encryption_algorithm=serialization. NoEncryption()
+#                                             )
+#     public_pem = public_key.public_bytes(encoding=serialization.Encoding.PEM,
+#                                          format=serialization.PublicFormat.SubjectPublicKeyInfo
+#                                         )
 
 
-    with open("private.pem", "w") as f:
-        f.write(private_pem)
+#     with open("private.pem", "w") as f:
+#         f.write(private_pem)
 
-    with open("public.pem", "w") as f:
-        f.write(public_pem)
+#     with open("public.pem", "w") as f:
+#         f.write(public_pem)
